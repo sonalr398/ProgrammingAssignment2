0cb41f0a72c9480f33a82710df114f088ea0ecc3
sonalr398 <47683897+sonalr398@users.noreply.github.com>
2020-05-24 13:32:56 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,15 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        j <- NULL
-        set <- function(y){
-                x <<- y
-                j <<- NULL
+        a <- NULL  # assignning null value to variable a 
+        set <- function(y){   # function to assign value of y to the variable x
+                x <<- y      #assignment expression
+                a <<- NULL   # local assignment of the variable a
         }
-        get<-function()x
-        setInverse<- function(inverse) j<<-inverse
-        getInverse <- function()j
-        list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
+        get<-function()x    # function to print the value of x
+        setInverse<- function(inverse) a<<-inverse  # anonymous function to obtain the inverse 
+        getInverse <- function()a   # anonymous function to output the inversed value
+        list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)  # creating a list of functions
 }
 
 
@@ -20,13 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        j <- x$getInverse()
-        if(!is.null(j)){
+        a <- x$getInverse()  # function call to the defined inverse function
+        if(!is.null(a)){
                 message("getting cached data")
-                return(j)
+                return(a)
         }
-        mat <- x$get
-        j<- solve(mat,...)
-        x$setInverse(j)
-        j
+        mat <- x$get    # function call to output the value of x
+        a<- solve(mat,...)
+        x$setInverse(a)  # function call to inverse the matric
+        a # returning the inversed matrix as the final output
 }
